c57e9ec9375efabd78ba8688f93530496da52c68
scipi <scipii48@gmail.com>
2021-01-06 22:42:32 -0500

    Removed initial example endpoints

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -9,30 +9,9 @@ mod catchers;
 pub mod db;
 mod endpoints;
 
-#[get("/world")]
-fn index() -> &'static str {
-    "Hello, world!"
-}
-
-#[get("/hello/<name>")]
-fn hello(name: String) -> String {
-    format!("Hello, {}!", name)
-}
-
-#[get("/hello/<first_name>?<last_name>&<greeting_noun>")]
-fn hello_full(first_name: String, last_name: String, greeting_noun: Option<String>) -> String {
-    let greeting_noun = greeting_noun
-        .map(|noun| format!("*{}* ", noun))
-        .unwrap_or_else(|| "".into());
-    format!("{}Hello, {} {}!", greeting_noun, first_name, last_name)
-}
-
 pub fn build_rocket() -> rocket::Rocket {
     let client = db::DBClient::init("mongodb://localhost:27017/");
     let routes = routes![
-        index,
-        hello,
-        hello_full,
         endpoints::signup::signup_endpoint,
         endpoints::login::login_endpoint,
         endpoints::user::self_endpoint,
